37bf23b7460f00e1cc077fd981d7f58ad476b90e
mrogowski01 <michal.rogowski2611@gmail.com>
2024-06-04 20:09:08 +0200

    initial commit

---
+++ src/main/java/com/example/zti_project/ZtiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.zti_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZtiProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZtiProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/zti_project/ZtiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zti_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZtiProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
